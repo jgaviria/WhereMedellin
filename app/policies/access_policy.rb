1be575ac54ac806d2ef625fa5526e843d6022e20
Juan Gaviria <jgaviria@clarityservices.com>
2018-09-22 02:51:32 -0400

    add some more roles

--- app/policies/access_policy.rb
+++ app/policies/access_policy.rb
@@ -4,13 +4,17 @@ class AccessPolicy
   def configure
     role :admin, proc { |user| user.admin? } do
       can :create, Event
+      can :edit, Event
+      can :update, Event
+      can :destroy, Event
+      can :create, User
     end
 
     role :cliente, proc { |user| user.client? } do
       can :create, Event
-        # can [:update, :destroy], Post do |post, user|
-        #   post.author == user
-        # end
+      # can [:update, :destroy], Post do |post, user|
+      #   post.author == user
+      # end
     end
   end
 end
